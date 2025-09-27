fe5d71de96070659bd95b197728cd435fbbb9890
zaky <zaky.cse@gmail.com>
2025-09-27 15:08:27 +0600

    commit variable

--- Javascript Class 9/js/custom.js
+++ Javascript Class 9/js/custom.js
@@ -1,14 +1,35 @@
 "use strict";
 
+//standard format if need to variable after some work
 let name;   //variable declare
 name = "allah";  //variable define
 
 alert(name);
 
-let firstname;
-firstname = "Zaky";
+let firstname = "Zaky"; //alternative variable declare & define
 
 alert(firstname);
 
 
+let firstname = "Zaky", bookname = "my book"; //same line use and declare define variable
 
+let firstname = "Zaky", 
+    bookname = "my book"; 
+
+
+let firstname = "Zaky";
+let bookname = "mybook";  //standard format
+let name = "Zaky
+
+//Possible Variable Call
+//symbol = $, _
+//letter = a-z, a-z
+//Digit = 1-10
+//my_name
+//my$name
+//myname1
+//myname2
+
+
+//variale using camel case
+//myName
