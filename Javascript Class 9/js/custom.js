02be62bff9fc1a48b22211aa0214c9fc6be4a9c8
zaky <zaky.cse@gmail.com>
2025-09-27 14:34:31 +0600

    commit 2nd

--- Javascript Class 9/js/custom.js
+++ Javascript Class 9/js/custom.js
@@ -1,11 +1,14 @@
-alert("Zaky");
+"use strict";
+
+let name;   //variable declare
+name = "allah";  //variable define
+
+alert(name);
+
+let firstname;
+firstname = "Zaky";
+
+alert(firstname);
 
 
 
-alert("Zaky");
-
-
-
-
-
-alert("Zaky");
